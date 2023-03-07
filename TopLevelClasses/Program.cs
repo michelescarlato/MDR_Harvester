e1ce432a7dfd92ac1ea29dafe2cb0a11251346e6
scanhamman <stevecanham@outlook.com>
2023-03-07 11:01:14 +0100

    t1

--- TopLevelClasses/Program.cs
+++ TopLevelClasses/Program.cs
@@ -61,7 +61,6 @@ if (paramsCheck.ParseError || paramsCheck.ValidityError)
 {
     // End program, parameter errors should have been logged
     // in a 'no source' file by the ParameterChecker class.
-
     return -1;
 }
 
